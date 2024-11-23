e345470d1eaadd0fa5d7351dca87be01024962be
doseeing <doseeing@gmail.com>
2024-11-23 23:46:21 +0800

    feat: update me api auth method

--- api/me.ts
+++ api/me.ts
@@ -1,13 +1,13 @@
 import jwt from "jsonwebtoken"
 
-const jwtSecret = "shhhhh"
+const jwtSecret = process.env.JWT_SECRET
 
 const successResult = {
   code: 200,
   message: "success",
   data: {
     id: 1,
-    username: "admin",
+    username: process.env.ADMIN_USERNAME,
     password: "",
     base_path: "/",
     role: 2,
@@ -28,6 +28,9 @@ export function GET(request: Request) {
   try {
     const token = request.headers.get("Authorization")
     if (!token) {
+      return new Response(JSON.stringify(failResult))
+    }
+    if (!jwtSecret) {
       return new Response(JSON.stringify(failResult))
     }
     let decoded = jwt.verify(token, jwtSecret)
